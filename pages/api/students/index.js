47b23541dd353ddf56c6cef8d39f7e93b3e21294
tami <xorud7979@gmail.com>
2022-09-23 13:53:11 +0900

    feat: 백엔드 코드에 get, delete 추가

--- pages/api/students/index.js
+++ pages/api/students/index.js
@@ -10,6 +10,9 @@ export default async function handler(req, res) {
       },
     } = req;
 
+    // 여기서 연산
+    
+
     console.log("백엔드 로그: ", studentName, studentGrade, kor, eng, math);
     const result = await client.student.create({
       data: {
@@ -20,7 +23,36 @@ export default async function handler(req, res) {
         math: parseInt(math),
       },
     });
-
     res.status(200).json(result);
   }
+
+  if (req.method === "GET") {
+    console.log("get 호출");
+
+    // const {
+    //   body: {
+    //     data: { studentName, studentGrade, kor, eng, math },
+    //   },
+    // } = req.query;
+
+    const result = await client.student.findMany({});
+    console.log(result);
+    res.status(200).json(result);
+  }
+
+  if (req.method === "DELETE") {
+    console.log("DELETE 호출");
+    const result = await client.student.delete({
+      data: {
+        studentName,
+        studentGrade: studentGrade,
+        kor: parseInt(kor),
+        eng: parseInt(eng),
+        math: parseInt(math),
+      },
+    });
+    res.status(200).json(result);
+  }
+
+  
 }
